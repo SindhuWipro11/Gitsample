c7d61d67db20e2ad75cebd1370d1885ec0046aef
SindhuWipro11 <sai.sindhimini11@gmail.com>
2023-10-27 15:29:23 +0530

    new code

---
+++ src/org/tcs/GitProgram.java
@@ -0,0 +1,12 @@
+package org.tcs;
+
+public class GitProgram {
+	public static void main(String[] args) {
+		System.out.println("My name is sindhu");
+		System.out.println("Updated Program");
+		System.out.println("Sidhu");
+	}
+	}
+
+
+
